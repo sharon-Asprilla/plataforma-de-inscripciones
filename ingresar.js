0aa5b1fa1cf03038c89679d1163d74b598984418
Juan Zapata <juanchozapatato@gmail.com>
2025-10-27 08:46:11 -0500

    nuevos cambios de colores

--- ingresar.js
+++ ingresar.js
@@ -1,10 +1,8 @@
 const boton = document.getElementById("cerrar");
 
-boton.style.backgroundColor = "red";
-boton.style.width="200px";
-boton.style.height="200px";
-boton.style.borderRadius="10px"; 
-boton.style.border="none"; 
+boton.style.Color = "red";
+boton.style.fontSize="30px";
+
  
 
 /*
